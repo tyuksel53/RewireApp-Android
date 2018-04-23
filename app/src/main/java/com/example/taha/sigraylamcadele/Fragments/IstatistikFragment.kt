48ffd41ef1e1c7b23731744dec21ec097673b3a7
Taha Yüksel <taha.yuksel053@gmail.com>
2018-04-23 17:21:13 +0300

    chart eklendi
    ...

--- app/src/main/java/com/example/taha/sigraylamcadele/Fragments/IstatistikFragment.kt
+++ app/src/main/java/com/example/taha/sigraylamcadele/Fragments/IstatistikFragment.kt
@@ -1,7 +1,7 @@
 package com.example.taha.sigraylamcadele.Fragments
 
 
-import android.opengl.Visibility
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
@@ -16,9 +16,18 @@ import com.example.taha.sigraylamcadele.Dialogs.SmokeDialog
 import com.example.taha.sigraylamcadele.Library.CircleDecorator
 import com.example.taha.sigraylamcadele.Library.Portal
 import com.example.taha.sigraylamcadele.Library.UserPortal
+import com.example.taha.sigraylamcadele.Model.GrafikAy
 import com.example.taha.sigraylamcadele.Model.UserDate
 
 import com.example.taha.sigraylamcadele.R
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.LargeValueFormatter
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.CalendarMode
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
@@ -26,6 +35,7 @@ import es.dmoral.toasty.Toasty
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.DateFormatSymbols
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -81,11 +91,13 @@ class IstatistikFragment : android.app.Fragment(),SmokeDialog.onSmokeCountEntere
     lateinit var calendar:MaterialCalendarView
     val userNewSelections = ArrayList<UserDate>()
     val userUpdateselections = ArrayList<UserDate>()
+    var barchart:BarChart?=null
     lateinit var btnSave:Button
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view =  inflater!!.inflate(R.layout.fragment_istatistik, container, false)
         var flag = false
+        barchart = null
         userNewSelections.clear()
         userUpdateselections.clear()
         calendar = view.findViewById(R.id.userCalandarView)
@@ -131,11 +143,9 @@ class IstatistikFragment : android.app.Fragment(),SmokeDialog.onSmokeCountEntere
 
                 updateResult?.clone()?.enqueue(object:Callback<String>{
                     override fun onFailure(call: Call<String>?, t: Throwable?) {
-                        var dummy = ""
                     }
 
                     override fun onResponse(call: Call<String>?, response: Response<String>?) {
-                        var dummy = ""
                     }
 
                 })
@@ -171,6 +181,13 @@ class IstatistikFragment : android.app.Fragment(),SmokeDialog.onSmokeCountEntere
                 {
                     responseBody = response?.body()
 
+
+                    if(responseBody!!.size > 0)
+                    {
+                        barchart = view.findViewById(R.id.chartGun)
+                        barchart!!.visibility = View.VISIBLE
+                        initBarChart()
+                    }
                     if(UserPortal.loggedInUser?.LastLoginTime != null)
                     {
                         val currentDay = CalendarDay.from(
@@ -360,6 +377,113 @@ class IstatistikFragment : android.app.Fragment(),SmokeDialog.onSmokeCountEntere
         return view
     }
 
+    private fun initBarChart() {
+        var listAy = ArrayList<GrafikAy>()
+        var ayNum = Portal.textToDate(responseBody!![0].Date!!).getMonth()
+        var deneme = GrafikAy()
+        deneme.AyNumara = ayNum
+        deneme.AyIsim = getMonthForInt(ayNum)
+        for(i in 0 until responseBody!!.size)
+        {
+            if(ayNum != Portal.textToDate(responseBody!![i].Date!!).getMonth())
+            {
+                listAy.add(deneme)
+                ayNum = Portal.textToDate(responseBody!![i].Date!!).getMonth()
+                deneme = GrafikAy()
+                deneme.AyNumara = ayNum
+                deneme.AyIsim = getMonthForInt(ayNum)
+
+            }
+            if(responseBody!![i].Type == 1)
+            {
+                deneme.GoodDay  = deneme.GoodDay + 1
+            }
+
+            if(responseBody!![i].Type == 2)
+            {
+                deneme.BadDay = deneme.BadDay + 1
+            }
+        }
+        val checkAy = listAy.find { x->x.AyNumara == deneme.AyNumara }
+        if( (deneme.BadDay != 0  || deneme.GoodDay != 0) && checkAy == null )
+        {
+            listAy.add(deneme)
+        }
+        val entriesGood =  ArrayList<BarEntry>()
+        val entriesBad = ArrayList<BarEntry>()
+        val isimler = ArrayList<String>()
+        for(i in 0 until listAy.size)
+        {
+            entriesGood.add(BarEntry(i.toFloat(), listAy[i].GoodDay.toFloat()))
+            entriesBad.add(BarEntry(i.toFloat(), listAy[i].BadDay.toFloat()))
+            isimler.add(listAy[i].AyIsim)
+        }
+
+        val barWidth: Float
+        val barSpace: Float
+        val groupSpace: Float
+
+        barWidth = 0.3f
+        barSpace = 0f
+        groupSpace = 0.4f
+        barchart!!.setDescription(null)
+        barchart!!.setPinchZoom(false)
+        barchart!!.setScaleEnabled(false)
+        barchart!!.setDrawBarShadow(false)
+        barchart!!.setDrawGridBackground(false)
+
+        val groupCount = isimler.size
+
+        val xVals =  isimler
+
+        var set1 =  BarDataSet(entriesGood, "Temiz Gün");
+        set1.setColor(ContextCompat.getColor(activity, R.color.barGreen));
+        var set2 = BarDataSet(entriesBad, "Kötü Gün");
+        set2.setColor(ContextCompat.getColor(activity, R.color.barRed));
+        var data =  BarData(set1, set2);
+        data.setValueTextColor(ContextCompat.getColor(activity, R.color.textColorPrimary))
+        data.setValueFormatter(LargeValueFormatter());
+        barchart!!.setData(data)
+        barchart!!.getBarData().setBarWidth(barWidth);
+        barchart!!.getXAxis().setAxisMinimum(0.toFloat());
+        barchart!!.getXAxis().setAxisMaximum(0 + barchart!!.getBarData().getGroupWidth(groupSpace, barSpace) * groupCount);
+        barchart!!.groupBars(0.toFloat(), groupSpace, barSpace);
+        barchart!!.getData().setHighlightEnabled(false);
+        barchart!!.invalidate();
+
+        var l = barchart!!.getLegend();
+        l.setTextColor(ContextCompat.getColor(activity, R.color.textColorPrimary))
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(true);
+        l.setYOffset(20f);
+        l.setXOffset(0f);
+        l.setYEntrySpace(0f);
+        l.setTextSize(8f);
+
+        var xAxis = barchart!!.getXAxis();
+        xAxis.setGranularity(1f);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setDrawGridLines(false);
+        xAxis.setAxisLineColor(ContextCompat.getColor(activity, R.color.textColorPrimary))
+        xAxis.setTextColor(ContextCompat.getColor(activity, R.color.textColorPrimary))
+        xAxis.setAxisLineColor(R.color.textColorPrimary)
+        xAxis.setAxisMaximum(6.toFloat());
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(IndexAxisValueFormatter(xVals));
+        //Y-axis
+        barchart!!.getAxisRight().setEnabled(false);
+        var leftAxis = barchart!!.getAxisLeft();
+        leftAxis.setValueFormatter(LargeValueFormatter());
+        leftAxis.setTextColor(ContextCompat.getColor(activity, R.color.textColorPrimary))
+        leftAxis.setAxisLineColor(ContextCompat.getColor(activity, R.color.textColorPrimary))
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setSpaceTop(35f);
+        leftAxis.setAxisMinimum(0f);
+    }
+
     fun getDrawable(type:Int):Int
     {
         when(type)
@@ -369,6 +493,16 @@ class IstatistikFragment : android.app.Fragment(),SmokeDialog.onSmokeCountEntere
             2 -> return R.drawable.circle_negative
             else -> return R.drawable.circle_gray
         }
+    }
+
+    fun getMonthForInt(num: Int): String {
+        var month = "wrong"
+        val dfs = DateFormatSymbols()
+        val months = dfs.getMonths()
+        if (num >= 0 && num <= 11) {
+            month = months[num]
+        }
+        return month
     }
 
 }// Required empty public constructor
---
+++ app/src/main/java/com/example/taha/sigraylamcadele/Model/GrafikAy.kt
@@ -0,0 +1,16 @@
+package com.example.taha.sigraylamcadele.Model
+
+class GrafikAy
+{
+    var AyIsim:String
+    var AyNumara:Int
+    var GoodDay:Int
+    var BadDay:Int
+
+    init {
+        AyIsim = ""
+        AyNumara = 0
+        GoodDay = 0
+        BadDay = 0
+    }
+}
